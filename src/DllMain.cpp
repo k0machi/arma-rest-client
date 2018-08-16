2a12c3d6985aee82372ea1b11cdaa6e8788ea5bd
Komachi <xenobyterus@gmail.com>
2018-08-17 00:43:51 +0300

    Dll Search Path handling
    Additional logging
    
    Delay loading of libcrypto and libssl

--- src/DllMain.cpp
+++ src/DllMain.cpp
@@ -1,7 +1,11 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <Shlwapi.h>
 #include <vector>
 #include "Worker.h"
+
+
+static const size_t gc_cbszModuleFilename = 512;
 
 extern std::vector<ozk::Worker*> g_ExtensionWorkers; //Defined in RVExtension.cpp
 
@@ -9,7 +13,17 @@ BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD reason, LPVOID reserved) {
 	switch (reason) {
 	case DLL_THREAD_ATTACH: break;
 	case DLL_THREAD_DETACH: break;
-	case DLL_PROCESS_ATTACH: break;
+	case DLL_PROCESS_ATTACH:
+	{
+		char szModuleFilename[gc_cbszModuleFilename];
+		const LPSTR pszModuleFilename = szModuleFilename;
+		auto cbszLength = GetModuleFileName(static_cast<HMODULE>(hInstance), pszModuleFilename, gc_cbszModuleFilename);
+		auto bSuccRemove = PathRemoveFileSpec(pszModuleFilename);
+		if (bSuccRemove) {
+			SetDllDirectory(pszModuleFilename);
+		}
+		break;
+	}
 	case DLL_PROCESS_DETACH:
 	{	//Terminate Extension's Worker Threads on Detach. Happens on closing the game.
 		for (auto && worker : g_ExtensionWorkers) {
